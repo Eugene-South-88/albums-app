551ccccbd8ae0206317aa15841068490b6c6abe0
Eugene <me.eugene.south@gmail.com>
2025-03-19 16:55:45 +0300

    feature (ASSIGNED_BY_ID, CREATED_BY_ID)

--- src/api/mappers/reportTableMap.js
+++ src/api/mappers/reportTableMap.js
@@ -1,14 +1,18 @@
 import {formatDateYMD} from "@/utils/helpers/dateHelper.js";
 
-export const reportTableMap = (table) => {
+export const reportTableMap = (table, users) => {
   return table.map((element) => {
+    const assigneeUser = users.find((user) => user.ID === element.ASSIGNED_BY_ID) || '';
+    const authorUser = users.find((user) => user.ID === element.CREATED_BY_ID) || '';
+
+
     return {
       ...element,
       DATE_CREATE: formatDateYMD(element.DATE_CREATE),
       CLOSEDATE: formatDateYMD(element.CLOSEDATE),
       STAGE_SEMANTIC_ID: stageSemanticMap[element.STAGE_SEMANTIC_ID],
-      ASSIGNED_BY_ID: 'Иван Иванов',
-      CREATED_BY_ID: 'Борис Моисеев'
+      ASSIGNED_BY_ID: `${assigneeUser?.LAST_NAME} ${assigneeUser?.NAME}`,
+      CREATED_BY_ID: `${assigneeUser?.LAST_NAME} ${assigneeUser?.NAME}`
     }
   })
 }
